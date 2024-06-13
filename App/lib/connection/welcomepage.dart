c668713c1a9791e5ff59c6a2a5772aaaacc807e2
remycharles <remy.charles@algosup.com>
2024-06-13 13:54:45 +0200

    redesign the welcome page

--- App/lib/connection/welcomepage.dart
+++ App/lib/connection/welcomepage.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import './loginpage.dart';
 import './signinpage.dart';
 import '../matchpage.dart';
@@ -10,59 +11,104 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(   // Background
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.blue, Colors.yellow],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-                child: Center( 
+      body: Stack(
+        children: [
+          // Background image with blur
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background_welcomepage.png'),
+                fit: BoxFit.cover, // Cover ensures the image covers the entire container
+                alignment: Alignment.center,
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+              child: Container(
+                color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.1),
+              ),
+            ),
+            width: double.infinity, // Ensure the container takes up the entire width of the screen
+            height: double.infinity, // Ensure the container takes up the entire height of the screen
+          ),
+          // Gradient overlay at the bottom
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  const Color.fromARGB(255, 255, 255, 255).withOpacity(0.7),
+                  Colors.transparent,
+                ],
+              ),
+            ),
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          // Centered content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: 500,
+                  height: 300,
+                  padding: const EdgeInsets.all(40.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container( // White box
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        width: 300,
-                        height: 250,
-                        padding: const EdgeInsets.all(40.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [ // Buttons
-                            SizedBox(
-                              width: 200.0,
-                              child: ElevatedButton( // to login
-                                onPressed: () => Navigator.of(context).push(LoginPage(0)),
-                                child: const Text('Se connecter'),
-                              ),
-                            ),
-                            const SizedBox(height: 40.0),
-                            SizedBox(
-                              width: 200.0,
-                              child: ElevatedButton( // to sign in
-                                onPressed: () => Navigator.of(context).push(SigninPage(0)),
-                                child: const Text('S\'inscrire'),
-                              ),
-                            ),
-                          ],
+                      SizedBox(
+                        width: 250.0,
+                        height: 40.0,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.of(context).push(SigninPage(0)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            shadowColor: Colors.transparent,
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),  
+                          ),
+                          child: const Text('Créer un compte', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
                         ),
                       ),
-                      const SizedBox(height: 40.0), // dev button
+                      const SizedBox(height: 10.0),
                       SizedBox(
                         width: 200.0,
+                        height: 25,
                         child: ElevatedButton(
-                            onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => MPage())),
-                          child: const Text('Passer(dev)'),
+                          onPressed: () => Navigator.of(context).push(LoginPage(0)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            foregroundColor: Colors.white,
+                            shadowColor: Colors.transparent,
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),  
+                          ),
+                          child: const Text(
+                            'Se connecter',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 35, 35, 35)),
+                          ),
                         ),
-                      ), // end dev button
+                      ),
                     ],
                   ),
                 ),
-              ),
-            );
-          }
+                const SizedBox(height: 50.0), // dev button
+                SizedBox(
+                  width: 200.0,
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => MPage())),
+                    child: const Text('Passer(dev)'),
+                  ),
+                ),
+                const SizedBox(height: 20.0), // end dev button
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
