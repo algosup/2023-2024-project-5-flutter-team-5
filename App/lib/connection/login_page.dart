9cc96f68268afe5e0e2fb5dc7a5c59723d9456bd
maxbernard3 <max.bernard_@hotmail.com>
2024-06-14 15:07:40 +0200

    removed unused import in login

--- App/lib/connection/login_page.dart
+++ App/lib/connection/login_page.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 //pages
